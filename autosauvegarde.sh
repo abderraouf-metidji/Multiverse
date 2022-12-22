bee1469ac672f077bef02258311e62fd05e3511f
vincent-panabieres <115145513+vincent-panabieres@users.noreply.github.com>
2022-12-22 18:07:12 +0100

    Delete autosauvegarde.sh

--- autosauvegarde.sh
+++
@@ -1,7 +0,0 @@
-sudo mkdir -p /home/userftp/ftp_directory
-sudo chown userftp:userftp /home/userftp/ftp_directory
-sudo chmod a-w /home/userftp/ftp_directory
-sudo mkdir -p /home/userftp/ftp_directory/ftp_data
-sudo chown userftp:userftp /home/userftp/ftp_directory/ftp_data
-cd /home/userftp/ftp_directory/
-chmod -R 777 ftp_data
